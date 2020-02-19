0847e3b91baa1997f516bddb7af0a9de1064be25
MikeKOD <52321149+MikeKOD@users.noreply.github.com>
2020-02-19 22:05:46 +0100

    Update hdmatches.py

--- testcalcio/hdmatches.py
+++ testcalcio/hdmatches.py
@@ -22,20 +22,8 @@ def mainlist(item):
       ]
       return itemlist
 from channels.support import log
-def mainlist(item):
-log()
-      itemlist = [Item(channel = item.channel,
-                       contentType = 'movie',
-                       title = 'Film',
-                       url = host + '/category/serie-a/',
-                       action = 'peliculas',
-                       thumbnail = '',
-                       fanart = ''
-      ),
-      ]
-return itemlist
 def peliculas(item):
-    log()
-    itemlist = []
-    data = httptools.downloadpage(item.url, headers=headers).data
-    block = scrapertools.find_single_match(data, r'<main>(.*?)<\/main>')
+      log()
+      itemlist = []
+      data = httptools.downloadpage(item.url, headers=headers).data
+      block = scrapertools.find_single_match(data, r'<main>(.*?)<\/main>')
